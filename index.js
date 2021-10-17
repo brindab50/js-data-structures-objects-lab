58d9856239d7d5621f015fe53573aa95ab297e41
Brinda Behal <brindaxo5@gmail.com>
2021-10-17 15:05:52 -0400

    commit tree b22ba343dfdc4e9ab4d5493592942e5379cbe5c2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){  // non-destructive 
+    return Object.assign({}, driver, {[key]: value})
+
+    
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){ // destructive 
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){  // non-destructive
+    const newObj = Object.assign({}, driver);
+
+    delete newObj[key];
+  
+    return newObj;
+
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {  // destructive 
+    delete driver[key];
+  
+    return driver;
+  }
